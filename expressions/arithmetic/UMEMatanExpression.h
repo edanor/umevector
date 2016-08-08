5b5462c11f610f32b863bf253378f5b809ab9062
Przemysław Karpiński <przemyslaw.karpinski@cern.ch>
2016-08-08 15:02:10 +0200

    Add masked unary arithmetic expression templates.

---
+++ expressions/arithmetic/UMEMatanExpression.h
@@ -0,0 +1,194 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 CERN
+//
+// Author: Przemyslaw Karpinski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+//  This piece of code was developed as part of ICE-DIP project at CERN.
+//  "ICE-DIP is a European Industrial Doctorate project funded by the European Community's
+//  7th Framework programme Marie Curie Actions under grant PITN-GA-2012-316596".
+//
+// ***This file has been automatically generated***
+//
+#ifndef UME_MATAN_EXPRESSION_H_
+#define UME_MATAN_EXPRESSION_H_
+
+namespace UME {
+namespace VECTOR {
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE> class ScalarExpression;
+
+    template<int SIMD_STRIDE, typename E1> class LogicalLNOTExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISFINExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISINFExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISANExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISNANExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISNORMExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISSUBExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISZEROExpression;
+    template<int SIMD_STRIDE, typename E1> class LogicalISZEROSUBExpression;
+
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalLANDExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalLORExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalLXORExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalLANDNOTExpression;
+
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPGTExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPLTExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPGEExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPLEExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPEQExpression;
+    template<int SIMD_STRIDE, typename E1, typename E2> class LogicalCMPNEExpression;
+
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticRCPExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticNEGExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticABSExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticSQRTExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticROUNDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticFLOORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticCEILExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticEXPExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticLOGExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticLOG10Expression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticLOG2Expression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticSINExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticCOSExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticTANExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticATANExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticBNOTExpression;
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticSADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticMULExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticDIVExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticSUBExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticSSUBExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticMAXExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticMINExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticBANDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticBORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticBXORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticBANDNOTExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2> class ArithmeticREMExpression;
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticBLENDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMSADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMMULExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMDIVExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMSUBExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMSSUBExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMMAXExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMMINExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMBANDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMBORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMBXORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK, typename E2> class ArithmeticMBANDNOTExpression;
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2, typename E3> class ArithmeticFMULADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2, typename E3> class ArithmeticFMULSUBExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2, typename E3> class ArithmeticFADDMULExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E2, typename E3> class ArithmeticFSUBMULExpression;
+
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticHADDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticHMULExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticHBANDExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticHBORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticHBXORExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticPOSTINCExpression;
+    template<typename SCALAR_TYPE, int SIMD_STRIDE, typename E1> class ArithmeticPOSTDECExpression;
+
+    template <typename SCALAR_TYPE, int SIMD_STRIDE, typename E1, typename E_MASK>
+    class ArithmeticMATANExpression :
+        public ArithmeticExpression<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK> >
+    {
+        typedef typename UME::SIMD::SIMDVec<SCALAR_TYPE, SIMD_STRIDE> SIMD_TYPE;
+        typedef typename UME::SIMD::SIMDVec<SCALAR_TYPE, 1> SIMD_1_TYPE;
+
+        E1 & _e1;
+        E_MASK & _e_mask;
+
+    public:
+        UME_FORCE_INLINE ArithmeticMATANExpression(E1 & e1, E_MASK & e_mask) :
+            _e1(e1),
+            _e_mask(e_mask) {}
+
+        UME_FORCE_INLINE ArithmeticMATANExpression(E1 && e1, E_MASK & e_mask) :
+            _e1(std::move(e1)),
+            _e_mask(e_mask) {}
+
+        UME_FORCE_INLINE ArithmeticMATANExpression(E1 & e1, E_MASK && e_mask) :
+            _e1(e1),
+            _e_mask(std::move(e_mask)) {}
+
+        UME_FORCE_INLINE ArithmeticMATANExpression(E1 && e1, E_MASK && e_mask) :
+            _e1(std::move(e1)),
+            _e_mask(std::move(e_mask)) {}
+
+        UME_FORCE_INLINE SIMD_TYPE evaluate_SIMD(int index)
+        {
+            auto t0 = _e1.evaluate_SIMD(index);
+            auto t1 = _e_mask.evaluate_SIMD(index);
+            auto t2 = t0.atan(t1);
+            return t2;
+        }
+
+        UME_FORCE_INLINE SIMD_1_TYPE evaluate_scalar(int index)
+        {
+            auto t0 = _e1.evaluate_scalar(index);
+            auto t1 = _e_mask.evaluate_scalar(index);
+            auto t2 = t0.atan(t1);
+            return t2;
+        }
+
+        typedef typename UTOITrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE UTOI_EXPRESSION_TYPE;
+        typedef typename UTOFTrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE UTOF_EXPRESSION_TYPE;
+        typedef typename ITOUTrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE ITOU_EXPRESSION_TYPE;
+        typedef typename ITOFTrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE ITOF_EXPRESSION_TYPE;
+        typedef typename FTOUTrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE FTOU_EXPRESSION_TYPE;
+        typedef typename FTOITrait<SCALAR_TYPE, SIMD_STRIDE, ArithmeticMATANExpression<SCALAR_TYPE, SIMD_STRIDE, E1, E_MASK>>::CAST_TYPE FTOI_EXPRESSION_TYPE;
+
+        UME_FORCE_INLINE UTOI_EXPRESSION_TYPE utoi() {
+            return UTOI_EXPRESSION_TYPE(*this);
+        }
+        UME_FORCE_INLINE UTOF_EXPRESSION_TYPE utof() {
+            return UTOF_EXPRESSION_TYPE(*this);
+        }
+        UME_FORCE_INLINE ITOU_EXPRESSION_TYPE itou() {
+            return ITOU_EXPRESSION_TYPE(*this);
+        }
+        UME_FORCE_INLINE ITOF_EXPRESSION_TYPE itof() {
+            return ITOF_EXPRESSION_TYPE(*this);
+        }
+        UME_FORCE_INLINE FTOU_EXPRESSION_TYPE ftou() {
+            return FTOU_EXPRESSION_TYPE(*this);
+        }
+        UME_FORCE_INLINE FTOI_EXPRESSION_TYPE ftoi() {
+            return FTOI_EXPRESSION_TYPE(*this);
+        }
+    };
+
+}
+}
+
+#endif
