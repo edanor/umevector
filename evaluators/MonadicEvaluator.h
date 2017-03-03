3a42a8d0c25cde02fc25baac00b59ecf2160c314
Przemyslaw Ryszard Karpinski <przemyslaw.karpinski@cern.ch>
2017-03-03 11:52:39 +0100

    Do not force inlining on MonadicEvaluator.

--- evaluators/MonadicEvaluator.h
+++ evaluators/MonadicEvaluator.h
@@ -17,11 +17,11 @@ public:
         int SIMD_STRIDE,
         typename DST_T,
         typename EXP_T>
-    UME_FORCE_INLINE MonadicEvaluator(
+        inline MonadicEvaluator(
         DST_T & dst,
         UME::VECTOR::ArithmeticExpression<SCALAR_TYPE, SIMD_STRIDE, EXP_T> & exp)
     {
-        EXP_T & reinterpret_exp = static_cast<EXP_T &>(exp1);
+        EXP_T & reinterpret_exp = static_cast<EXP_T &>(exp);
 
         for (int i = 0; i < reinterpret_exp.LOOP_PEEL_OFFSET(); i += SIMD_STRIDE) {
             UME::SIMD::SIMDVec<SCALAR_TYPE, SIMD_STRIDE> t0 = reinterpret_exp.evaluate_SIMD(i);
@@ -42,13 +42,13 @@ public:
         typename SCALAR_TYPE,
         int SIMD_STRIDE,
         typename EXP_T>
-    UME_FORCE_INLINE MonadicEvaluator(
+        inline MonadicEvaluator(
             SCALAR_TYPE & dst,
             UME::VECTOR::ArithmeticExpression<SCALAR_TYPE, SIMD_STRIDE, EXP_T> & exp)
     {
         EXP_T & reinterpret_exp = static_cast<EXP_T &>(exp);
-
-        dst = exp1.evaluate_scalar(0);
+        UME::SIMD::SIMDVec<SCALAR_TYPE, 1> t1 = reinterpret_exp.evaluate_scalar(0);
+        dst = t1[0];
     }
 
     // Evaluate expression with no destination, but side-effects (e.g. destructive ADD operation).
@@ -56,7 +56,7 @@ public:
         typename SCALAR_TYPE,
         int SIMD_STRIDE,
         typename EXP_T>
-        UME_FORCE_INLINE MonadicEvaluator(
+        inline MonadicEvaluator(
             UME::VECTOR::ArithmeticExpression<SCALAR_TYPE, SIMD_STRIDE, EXP_T> & exp)
     {
         // Need to reinterpret vec to E to propagate to proper expression
